c9b541c6521194a382ebb0c89cc688712c0d416c
fly-1-1 <2673024411@qq.com>
2024-03-05 11:27:05 +0800

    员工分页查询

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -2,7 +2,9 @@ package com.sky.service;
 
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
@@ -13,5 +15,19 @@ public interface EmployeeService {
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 
+    /**
+     * 新增员工
+     *
+     * @param employeeDTO 员工dto
+     */
     void save(EmployeeDTO employeeDTO);
+
+    /**
+     * 页面查询
+     *
+     * @param employeePageQueryDTO 员工页面查询dto
+     * @return {@link PageResult}
+     */
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
 }
